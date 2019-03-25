4bb2f996a81de522ef3c330fa66e48b0352e11af
dimerick <ericksaenz@outlook.com>
2019-03-24 21:51:56 -0500

    v1.0

---
+++ app/admin.py
@@ -0,0 +1,43 @@
+from django.contrib import admin
+
+from .models import Slider, Slide, Partner, TeamMember, Activity
+
+# Register your models here.
+class SliderAdmin(admin.ModelAdmin):
+	list_display = ('id', 'name')
+
+class SlideAdmin(admin.ModelAdmin):
+	list_display = ('title', 'is_active', 'position', 'slider')
+
+	fieldsets = (
+        ('Title', {
+            'fields': ('title', ('top_title', 'left_title'), ('size_title', 'color_title'))
+        }),
+        ('Description', {
+            'fields': ('text', ('top_text', 'left_text'), ('size_text', 'color_text'))
+        }),
+        ('Image', {
+            'fields': (['image'])
+        }),
+        ('Button', {
+            'fields': ('show_button', ('text_button', 'url_button', 'color_button'), ('top_button', 'left_button'))
+        }),
+        ('General Options', {
+            'fields': ('slider', 'is_active', 'position')
+        })
+    )
+
+class TeamMemberAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'position')
+	# fields = ('slider', 'image', 'title', ('top_title', 'left_title', 'size_title'), 'text', 
+	# 	('top_text', 'left_text', 'size_text'), ('show_button', 'text_button', 'url_button'), ('top_button', 
+	# 		'left_button'), 'is_active', 'position')
+
+class ActivityAdmin(admin.ModelAdmin):
+    list_display = ('name', 'is_active', 'date')
+
+admin.site.register(Slider, SliderAdmin)
+admin.site.register(Slide, SlideAdmin)
+admin.site.register(Partner)
+admin.site.register(TeamMember, TeamMemberAdmin)
+admin.site.register(Activity, ActivityAdmin)
